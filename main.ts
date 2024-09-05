ad0b78498524cf915c7fe1418d1343f0cfba6aec
amitkrvarshney <amitkrvarshney@gmail.com>
2024-09-06 01:10:21 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let E_name = game.askForString("E_name ",12)
+let E_address = game.askForString("E_address ",12)
+let E_id = game.askForString("E_id ",12)
+{
+
+game.splash("E_name",E_name)
+game.splash("E_address",E_address)
+    game.splash("E_id",E_name)
+}
